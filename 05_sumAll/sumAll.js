aed920de244f0e2fa8647bf6f16f5784606521f8
Matias Candi <63614139+matiascandi@users.noreply.github.com>
2024-02-15 17:36:20 +0100

    6 exercises done

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
-
+const sumAll = function(start, end) {
+    let totalSum = 0;
+    const array = [];
+    if (start > end){
+        [start, end] = [end, start];
+    }
+    if (typeof start != "number" || typeof end != "number" || start < 0 || end < 0){
+        return "ERROR";
+    }
+    for (let i = 0; i <= end; i++){
+        array.push(i);
+    }
+    for (let i = 0; i < array.length; i++){
+        totalSum += array[i];
+    }
+    return totalSum;
 };
 
 // Do not edit below this line
