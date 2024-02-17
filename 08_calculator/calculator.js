61ecdcee9e818ce21d62a33c4d36d59c199df9ab
Matias Candi <63614139+matiascandi@users.noreply.github.com>
2024-02-17 13:42:35 +0100

    finished all exercises

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -8,31 +8,46 @@ const subtract = function(x, y) {
   return result;
 };
 
-const sum = function() {
-	
-};
+const sum = function(array) {
+  const result = array.reduce((accumulator, currentValue) => {
+    return accumulator + currentValue;
+  }, 0);
 
-const multiply = function() {
-  for (let i = 0; i < Array.length; i++){
-
-  }
-  let result = x * y;
   return result;
 };
 
-const power = function(x) {
+const multiply = function(array) {
+  const result = array.reduce((accumulator, currentValue) => {
+    return accumulator * currentValue;
+  }, 1);
+
+  return result;
+};
+
+
+const power = function(x, y) {
 	let result;
   if (x === 0) {
     result = 1;
   }
   else {
-    result = Math.pow(x, x);
+    result = Math.pow(x, y);
   }
   return result;
 };
 
-const factorial = function() {
-	
+const factorial = function(x) {
+  const array = [];
+  if (x === 0 || x === 1){
+    return 1;
+  }
+  for (let i = 1; i <= x; i++){
+    array.push(i);
+  }
+  const fact = array.reduce ((accumulator, currentValue) => {
+    return accumulator * currentValue;
+  })
+  return fact;
 };
 
 // Do not edit below this line
